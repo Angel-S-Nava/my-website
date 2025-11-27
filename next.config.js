f27a8ccfe91dce950ec3040b582d9584fdfa9b79
Angel-S-Nava <angelnavafierro@gmail.com>
2025-11-27 17:20:25 -0600

    remove static export; enable serverless api

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
+  // NO output: 'export'  <-- importante eliminar si quieres funciones serverless
 };
 
 module.exports = nextConfig;
